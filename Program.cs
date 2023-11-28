8599a7ab4a5273c0fdbf738852d4926d75cf2796
Angelina-Saeteurn-Student <144161612+Angelina-Saeteurn-Student@users.noreply.github.com>
2023-11-28 18:07:30 +0000

    Made a function that will randomly choose names of students.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine(PickName());
+    }
+    public static String PickName(){
+
+            Random rand = new Random();
+            int num = rand.Next(0,25);
+            string[] names = {"Tyler 'TK' Kerkstra", "Ashley Berna", "Max Baykowski", "Hayden Flick", "Jacob Daniel", "Blair Ciolk", "Raymond Meara-Klinge", "Joshua Burgenmeyer",
+                            "Nick Fultz", "Ashley 'AJ' Haltenhoff", "Dakota McDaniel", "Kamren Moomey", "Keegan Ohlman", "Eli Robinson", "Jay V.", "Alec Tackitt", "Maximilian Wallace",
+                            "Keagan Ziemann-Bell", "Nick Johnson", "Ethan Witter", "Gavin Loocks", "Aedan Vonysouthi" , "Brandon Olsen", "Dylan Walters", "Angelina Saeteurn"};
+            string chosen= names[num];
+        return chosen;
+    }
+}
+
+
