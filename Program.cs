f4c17bd90f63c1f8e1df1ccd24b6cf746dcfeacb
Angelina-Saeteurn-Student <144161612+Angelina-Saeteurn-Student@users.noreply.github.com>
2023-12-01 17:32:47 +0000

    Made the suspense random, and added the robot to box breathing

--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
-        Console.WriteLine(PickName());
+        //TODO: ensure that no person can be chosen twice...
+        string name = PickName();
+        Console.WriteLine(name);
+        if (name=="Robot"){
+            RobotBreathing();
+        }
     }
     public static String PickName(){
 
@@ -16,18 +20,71 @@ public class Program
             string[] names = {"Tyler 'TK' Kerkstra", "Ashley Berna", "Max Baykowski", "Hayden Flick", "Jacob Daniel", "Blair Ciolk", "Raymond Meara-Klinge", "Joshua Burgenmeyer",
                             "Nick Fultz", "Ashley 'AJ' Haltenhoff", "Dakota McDaniel", "Kamren Moomey", "Keegan Ohlman", "Eli Robinson", "Jay V.", "Alec Tackitt", "Maximilian Wallace",
                             "Keagan Ziemann-Bell", "Nick Johnson", "Ethan Witter", "Gavin Loucks", "Aedan Vongsouthi" , "Brandon Olsen", "Dylan Walters", "Angelina Saeteurn", "Mr. Martinez", 
-                            "Mr.Luyk"};
+                            "Mr.Luyk", "Robot"};
             int num = rand.Next(0, names.Length);
             string chosen = names[num];
+            num = rand.Next(0, 20);
             Console.Write("Selcting the Box Breather");
+            for (int i=num; i>=0; i--){
             Console.Write(".");
             Thread.Sleep(1000);
-            Console.Write(".");
-            Thread.Sleep(1000);
-            Console.Write(". ");
-            Thread.Sleep(1000);
+            }
 
         return chosen;
+    }
+    public static void RobotBreathing(){
+        Console.WriteLine("Alright my turn");
+        Wait4();
+        Console.WriteLine("Inhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+        Console.WriteLine("Exhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+        Console.WriteLine("Inhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+        Console.WriteLine("Exhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+        Console.WriteLine("Inhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+        Console.WriteLine("Exhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+        Console.WriteLine("Inhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+        Console.WriteLine("Exhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+        Console.WriteLine("Inhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+        Console.WriteLine("Exhale");
+        Wait4();
+        Console.WriteLine("Hold it");
+        Wait4();
+    }
+    public static void Wait4(){
+        Console.Write(".");
+        Thread.Sleep(1000);
+        Console.Write(".");
+        Thread.Sleep(1000);
+        Console.Write(".");
+        Thread.Sleep(1000);
+        Console.WriteLine(".");
+        Thread.Sleep(1000);
     }
 }
 
