9b238a5b86f9121e432b965d11f06efdf242895b
Emmanuel Martinez <mmartine1@oaisd.org>
2023-12-05 11:16:54 -0500

    Fix mispelling

--- Program.cs
+++ Program.cs
@@ -9,30 +9,34 @@ public class Program
         //TODO: ensure that no person can be chosen twice...
         string name = PickName();
         Console.WriteLine(name);
-        if (name=="Robot"){
+        if (name == "Robot")
+        {
             RobotBreathing();
         }
     }
-    public static String PickName(){
+    public static String PickName()
+    {
 
-            Random rand = new Random();
-           
-            string[] names = {"Tyler 'TK' Kerkstra", "Ashley Berna", "Max Baykowski", "Hayden Flick", "Jacob Daniel", "Blair Ciolk", "Raymond Meara-Klinge", "Joshua Burgenmeyer",
+        Random rand = new Random();
+
+        string[] names = {"Tyler 'TK' Kerkstra", "Ashley Berna", "Max Baykowski", "Hayden Flick", "Jacob Daniel", "Blair Ciolk", "Raymond Meara-Klinge", "Joshua Burgenmeyer",
                             "Nick Fultz", "Ashley 'AJ' Haltenhoff", "Dakota McDaniel", "Kamren Moomey", "Keegan Ohlman", "Eli Robinson", "Jay V.", "Alec Tackitt", "Maximilian Wallace",
-                            "Keagan Ziemann-Bell", "Nick Johnson", "Ethan Witter", "Gavin Loucks", "Aedan Vongsouthi" , "Brandon Olsen", "Dylan Walters", "Angelina Saeteurn", "Mr. Martinez", 
+                            "Keagan Ziemann-Bell", "Nick Johnson", "Ethan Witter", "Gavin Loucks", "Aedan Vongsouthi" , "Brandon Olsen", "Dylan Walters", "Angelina Saeteurn", "Mr. Martinez",
                             "Mr.Luyk", "Robot"};
-            int num = rand.Next(0, names.Length);
-            string chosen = names[num];
-            num = rand.Next(0, 20);
-            Console.Write("Selcting the Box Breather");
-            for (int i=num; i>=0; i--){
+        int num = rand.Next(0, names.Length);
+        string chosen = names[num];
+        num = rand.Next(0, 20);
+        Console.Write("Selecting the Box Breather");
+        for (int i = num; i >= 0; i--)
+        {
             Console.Write(".");
             Thread.Sleep(1000);
-            }
+        }
 
         return chosen;
     }
-    public static void RobotBreathing(){
+    public static void RobotBreathing()
+    {
         Console.WriteLine("Alright my turn");
         Wait4();
         Console.WriteLine("Inhale");
@@ -76,7 +80,8 @@ public class Program
         Console.WriteLine("Hold it");
         Wait4();
     }
-    public static void Wait4(){
+    public static void Wait4()
+    {
         Console.Write(".");
         Thread.Sleep(1000);
         Console.Write(".");
