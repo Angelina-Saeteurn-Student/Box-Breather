8f1d1d7df806cd19217056094487a59107293055
Emmanuel Martinez <mmartine1@oaisd.org>
2024-02-05 11:30:15 -0500

    Update class roster

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ public class Program
         Random rand = new Random();
 
         string[] names = {"Tyler 'TK' Kerkstra", "Ashley Berna", "Max Baykowski", "Hayden Flick", "Jacob Daniel", "Blair Ciolk", "Raymond Meara-Klinge", "Joshua Burgenmeyer",
-                            "Nick Fultz", "Ashley 'AJ' Haltenhoff", "Dakota McDaniel", "Kamren Moomey", "Keegan Ohlman", "Eli Robinson", "Jay V.", "Alec Tackitt", "Maximilian Wallace",
+                            "Nick Fultz", "Ashley 'AJ' Haltenhoff", "Dakota McDaniel", "Keegan Ohlman", "Eli Robinson", "Jay V.", "Alec Tackitt", "Maximilian Wallace",
                             "Keagan Ziemann-Bell", "Nick Johnson", "Ethan Witter", "Gavin Loucks", "Aedan Vongsouthi" , "Brandon Olsen", "Dylan Walters", "Angelina Saeteurn", "Mr. Martinez",
                             "Mr.Luyk", "Robot"};
         int num = rand.Next(0, names.Length);
